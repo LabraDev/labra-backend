b859b3482c6d1fc1d9591a8cb812dd3e4fdb936d
Mark Gaballa <mgaballa@csu.fullerton.edu>
2026-03-12 14:39:31 -0700

    rm flake nix

--- flake.nix
+++
@@ -1,9 +0,0 @@
-{
-	inputs = {
-		nixpkgs.url = "github@nixos/nixpkgs";
-	};
-
-	outputs = { self, nixpkgs }: {
-		packages.aarch64-darwin.hello = nixpkgs.legacyPackages.aarch64-darwin.hello;
-	};
-}
